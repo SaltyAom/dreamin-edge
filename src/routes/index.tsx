267d95343c940913c64fc8ee44503a8ff7f2174d
SaltyAom <aomkirby@mystiar.com>
2020-09-06 13:33:48 +0700

    :tada: Add lazy render

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
-import { h } from 'preact'
-import { useMemo } from 'preact/hooks'
+import { h, Fragment } from 'preact'
+import { useMemo, useEffect, useState, useCallback, useRef } from 'preact/hooks'
 
 import { useStoreon } from 'storeon/preact'
 
@@ -14,25 +14,64 @@ import { SearchStore, SearchEvent } from '../store/search/types'
 
 import '../styles/menu-list.styl'
 
+const initialDisplayMenu = 20
+
 const Index = () => {
     let { search } = useStoreon<SearchStore, SearchEvent>('search'),
         { sort } = useStoreon<SortStore, SortEvent>('sort'),
         { dreamin } = useStoreon<DreaminStore, DreaminEvent>('dreamin')
+
+    let [totalDisplayMenu, updateTotalDisplayMenu] = useState(
+        initialDisplayMenu
+    )
+
+    // Reference to remove event listener
+    let previousLazyload = useRef<() => void>(null)
 
     let menuList = useMemo(
         () => applySort(filterSearch(dreamin, search), sort),
         [dreamin, search]
     )
 
+    let lazyLoadMenu = useCallback(() => {
+        let pageHeight = window.innerHeight
+
+        if (
+            totalDisplayMenu > menuList.length ||
+            document.body.scrollHeight >= window.pageYOffset + pageHeight * 1.75
+        )
+            return
+
+        updateTotalDisplayMenu(totalDisplayMenu + 20)
+    }, [totalDisplayMenu, menuList])
+
+    useEffect(() => {
+        updateTotalDisplayMenu(initialDisplayMenu)
+    }, [search])
+
+    useEffect(() => {
+        if (previousLazyload.current)
+            document.removeEventListener('scroll', previousLazyload.current)
+
+        if (totalDisplayMenu < menuList.length)
+            document.addEventListener('scroll', lazyLoadMenu, {
+                passive: true
+            })
+
+        previousLazyload.current = lazyLoadMenu
+    }, [totalDisplayMenu, menuList])
+
     if (!dreamin.length)
         return (
-            <ul id="menu-list">
-                {Array(24)
-                    .fill(null)
-                    .map((_, index) => (
-                        <Card key={index} index={index} preload />
-                    ))}
-            </ul>
+            <Fragment>
+                <ul id="menu-list">
+                    {Array(20)
+                        .fill(null)
+                        .map((_, index) => (
+                            <Card key={index} index={index} preload />
+                        ))}
+                </ul>
+            </Fragment>
         )
 
     if (!menuList.length)
@@ -52,22 +91,26 @@ const Index = () => {
 
     return (
         <ul id="menu-list">
-            {menuList.map(({ name, subMenu, price }, index) => (
-                <Card
-                    key={
-                        typeof name === 'undefined'
-                            ? subMenu[0]
-                            : `${name.th}-${name.jp}-${price}`
-                    }
-                    name={
-                        typeof name === 'undefined'
-                            ? subMenu
-                            : [name.th, name.en, name.jp]
-                    }
-                    price={price}
-                    index={index}
-                />
-            ))}
+            {menuList.map(({ name, subMenu, price }, index) => {
+                if (index >= totalDisplayMenu) return null
+
+                return (
+                    <Card
+                        key={
+                            typeof name === 'undefined'
+                                ? subMenu[0]
+                                : `${name.th}-${name.jp}-${price}`
+                        }
+                        name={
+                            typeof name === 'undefined'
+                                ? subMenu
+                                : [name.th, name.en, name.jp]
+                        }
+                        price={price}
+                        index={index}
+                    />
+                )
+            })}
         </ul>
     )
 }
